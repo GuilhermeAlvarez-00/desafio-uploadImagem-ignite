dd138b5dfb0fccb6b25e6961a14b6d86e3068b10
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-05-04 15:56:26 -0300

    listando imagens na tela

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { Grid, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -21,10 +21,22 @@ export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO SELECTED IMAGE URL STATE
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string) {
+    console.log(url);
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <Grid templateColumns={'repeat(3, 1fr)'} gap="40px">
+        {cards?.map(card => (
+          <Card
+            key={card.id}
+            data={card}
+            viewImage={() => handleViewImage(card.url)}
+          />
+        ))}
+      </Grid>
 
       {/* TODO MODALVIEWIMAGE */}
     </>
